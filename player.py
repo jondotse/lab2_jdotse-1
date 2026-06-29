b936ec693f5a1401f556b46b1bd1f9bbca2e3247
JonD <sethdotse05@gmail.com>
2026-06-28 23:40:45 -0400

    Added toss method and get coin method

--- player.py
+++ player.py
@@ -12,3 +12,21 @@ class Player:
         self.__name = name
         self.__wallet = 20
         self.__coin = Coin()
+
+    def toss_coin(self):
+        self.__coin.toss()
+
+    def get_coin_side(self):
+        return self.__coin.get_sideup()
+    
+    def win_coin(self):
+        self.__wallet += 1
+
+    def lose_coin(self):
+        self.__wallet -= 1
+
+    def get_wallet(self):
+        return self.__wallet
+    
+    def get_name(self):
+        return self.__name
